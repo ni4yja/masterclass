0799cb6d5374f674ecbe36ab2d9152d2339a242b
ni4yja <k2.liuba@gmail.com>
2022-05-05 22:23:48 +0300

    firestore issue

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import AppDate from './components/AppDate.vue';
 import router from './router';
-import store from './store'
+import store from './store';
+import firebase from 'firebase/app';
+import firebaseConfig from './config/firebase';
 
 import './style.css';
+
+firebase.initializeApp(firebaseConfig);
 
 const forumApp = createApp(App);
 forumApp.use(router);
